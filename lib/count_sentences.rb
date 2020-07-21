9a22ef83c69543d9454da963dd9108427cc1e435
William Han Findiesen <william.findiesen@gmail.com>
2020-07-21 04:41:41 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
+      
   end
 
   def question?
-
+  if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+  if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    newarray = []
+    newarray = self.split(/[.?!]/)
+    newarray= newarray.map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}
+    newarray.count
   end
 end
